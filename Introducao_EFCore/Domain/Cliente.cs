e1b77b21b65153a3af5ed4c551684862cdb62bc3
flavioflnlima <flavioflnlima@gmail.com>
2021-01-16 16:16:48 -0300

    criando arquivos de configurações para o modelo de dados - desacoplando o código

--- Introducao_EFCore/Domain/Cliente.cs
+++ Introducao_EFCore/Domain/Cliente.cs
@@ -5,7 +5,7 @@ namespace EFcore.Domain
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Telefone { get; set; }
-        public string Cep { get; set; }
+        public string CEP { get; set; }
         public string Estado { get; set; }
         public string Cidade { get; set; }
     }
