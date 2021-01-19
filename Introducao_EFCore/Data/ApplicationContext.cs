f22be759e7e743ae44e366895077a472ded2e60a
flavioflnlima <flavioflnlima@gmail.com>
2021-01-19 20:58:51 -0300

    inserindo dados em massa

--- Introducao_EFCore/Data/ApplicationContext.cs
+++ Introducao_EFCore/Data/ApplicationContext.cs
@@ -1,18 +1,23 @@
 using EFcore.Data.Configurations;
 using EFcore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFcore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = SQNOT11813\\SQLEXPRESS;Initial Catalog=CursoEFCore; Integrated Security=true");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server = SQNOT11813\\SQLEXPRESS;Initial Catalog=CursoEFCore; Integrated Security=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Introducao_EFCore/Program.cs
+++ Introducao_EFCore/Program.cs
@@ -9,7 +9,34 @@ namespace EFcore
     {
         static void Main(string[] args)
         {
-            InserirDados();
+            // InserirDados();
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaVenda,
+                Ativo = true
+            };
+
+            var Cliente = new Cliente
+            {
+                Nome = "Rafael Almeida",
+                CEP = "9999999",
+                Cidade = "Paraíso",
+                Estado = "TO",
+                Telefone = "63999999999",
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, Cliente);
+            var registros = db.SaveChanges();
+            Console.WriteLine($"registros = {registros}");
         }
 
         private static void InserirDados()
