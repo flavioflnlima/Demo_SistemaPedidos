45abb7bc2e8d5430bdcc78ece5bc976fec5105d7
flavioflnlima <flavioflnlima@gmail.com>
2021-01-19 20:39:00 -0300

    Inserindo Registros

--- Introducao_EFCore/Program.cs
+++ Introducao_EFCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using EFcore.Domain;
+using EFcore.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFcore
@@ -7,7 +9,29 @@ namespace EFcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaVenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            // db.Produtos.Add(produto);
+            // db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Toral Registros: {registros}");
+
         }
     }
 }
