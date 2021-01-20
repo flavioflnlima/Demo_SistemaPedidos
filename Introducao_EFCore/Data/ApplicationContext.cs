696c54f302fc51435dd2c66ce8a8cc739a62fe8b
flavioflnlima <flavioflnlima@gmail.com>
2021-01-20 19:59:07 -0300

    Detectando prop não configuradas, resiliencia de conexao

--- Introducao_EFCore/Data/ApplicationContext.cs
+++ Introducao_EFCore/Data/ApplicationContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using EFcore.Data.Configurations;
 using EFcore.Domain;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +10,8 @@ namespace EFcore.Data
     public class ApplicationContext : DbContext
     {
         private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        private readonly TimeSpan TimeSpan;
+
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
@@ -17,12 +21,34 @@ namespace EFcore.Data
             optionsBuilder
                 .UseLoggerFactory(_logger)
                 .EnableSensitiveDataLogging()
-                .UseSqlServer("Server = SQNOT11813\\SQLEXPRESS;Initial Catalog=CursoEFCore; Integrated Security=true");
+                .UseSqlServer("Server = SQNOT11813\\SQLEXPRESS;Initial Catalog=CursoEFCore; Integrated Security=true",
+                p => p.EnableRetryOnFailure(
+                    maxRetryCount: 2,
+                    maxRetryDelay: TimeSpan.FromSeconds(5),
+                    errorNumbersToAdd: null));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropiedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropiedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+                foreach (var property in properties)
+                {
+                    if (string.IsNullOrEmpty(property.GetColumnType())
+                        && !property.GetMaxLength().HasValue)
+                    {
+                        // property.SetMaxLength(100);
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
         }
     }
 }
